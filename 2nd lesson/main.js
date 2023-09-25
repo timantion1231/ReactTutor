e3d0c229cf098930e8497d05fb7dfc26044ee1f5
Khanafin Timur <mindiisky@yandex.ru>
2023-09-25 17:28:10 +0300

    trying to slove

--- 2nd lesson/main.js
+++ 2nd lesson/main.js
@@ -1,28 +1,45 @@
 let a = Math.floor(Math.random() * 100);
 
 function if_statement(a) {
-temp = 0
+    state1 = 0
+    state2 = 0
+    state3 = 0
+
     if (a > 10) {
-        temp = a
+        state1 = a
     }
     else {
-        temp = a*2
+        state1 = a * 2
     }
 
-    if(temp>5){
-        temp = (2*a)+1
+    if(state1>5){
+        state2 = (2*a)+1
     }
     else{
         if(a<3){
-            
+            state2 = 1
         }
         else{
-
+            state2 = 2*(a-2)
         }
     }
+
+    if(state2>4){
+        state3 = 5
+    }
+    else{
+        if(a%2==0){
+            state3 = 6
+        }
+        else{
+            state3 = 7
+        }
+    }
+
+    return state3
 }
 
-(a > 10 
+/*(a > 10 
     ? a 
     : a * 2) > 5 
         ? (2 * a) + 1 
@@ -34,14 +51,14 @@ temp = 0
                 :(a % 2 == 0 
                     ? 6 
                     : 7)
-
+*/
 
 
 function if_statement2(a) {
     return (a > 10 ? a : a * 2) > 5 ? (2 * a) + 1 : (a < 3 ? 1 : 2 * (a - 2)) > 4 ? 5 : (a % 2 == 0 ? 6 : 7)
 }
-console.log(a)
-console.log(if_statement2(a))//true
-console.log(if_statement(a))//check
+console.log("a: " + a)
+console.log("Tern original: " + if_statement2(a))//true
+console.log("if statement: " + if_statement(a))//check
 
 //console.log((a > 10 ? a : a * 2) > 5 ? (2 * a) + 1 : (a < 3 ? 1 : 2 * (a - 2)) > 4 ? 5 : (a % 2 == 0 ? 6 : 7));
